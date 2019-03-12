e004c4b593a9460637636f30868e2da6b2bea0fd
Arindo Mudiare <arindoTmudiare@gmail.com>
2019-03-12 18:02:32 +0100

    Api completed with some errors in the templating frontend

---
+++ Members.js
@@ -0,0 +1,23 @@
+// hardcoded json
+const members = [
+    {
+        id: 1,
+        name: 'Saraki',
+        email: 'saraki@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2, 
+        name: 'Tinubu',
+        email: 'tinubu@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Amaechi',
+        email: 'amaechi@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
